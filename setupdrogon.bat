86b8c977e2977e36c2c626012796a775a96bd350
TGyAkos <t.gyula.akos@gmail.com>
2024-11-27 09:58:35 +0100

    added vcpkg

---
+++ setupdrogon.bat
@@ -0,0 +1,5 @@
+git submodule update --init
+mkdir build
+cd build
+conan profile detect --force
+conan install .. -s compiler="msvc" -s compiler.version=193  -s compiler.cppstd=17 -s build_type=Debug  --output-folder . --build=missing
